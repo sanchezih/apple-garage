c07a08e22ee398dbe7f3e70dbb4ab5f42bac9c1e
mcaprino1 <mcaprino@ar.ibm.com>
2015-11-30 11:17:08 -0300

    controllers va tomando algo de forma

--- carrito/src/main/java/ar/com/ibm/applegarage/carrito/controller/AlmacenController.java
+++ carrito/src/main/java/ar/com/ibm/applegarage/carrito/controller/AlmacenController.java
@@ -1,5 +1,77 @@
 package ar.com.ibm.applegarage.carrito.controller;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import ar.com.ibm.applegarage.carrito.model.Producto;
+import ar.com.ibm.applegarage.carrito.service.AlmacenService;
+
+@Path("/stock")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AlmacenController {
 
+	
+	AlmacenService svc = new AlmacenService();
+	
+	@Path("/items")
+	@GET
+	public void getStockItems(){
+		
+	}
+	
+	@Path("/items")
+	@POST
+	public void addStockItem(Producto producto,
+								@QueryParam("cant") Long cant){
+		if (cant.intValue()<=0){
+			cant = 1L;
+		}
+		svc.agregarItem(producto, cant.intValue());
+		
+	}
+	
+	
+//	este metodo modifica el producto, y opcionalmente su cantidad
+	@Path("/items/{idProducto}")
+	@PUT
+	public void updateStockItem(@PathParam("idProducto") String id,
+								@QueryParam("cant") Long cant,
+								Producto producto){
+		
+//		quizas se quiera modificar el ID del producto, asique especificar el targetid.
+//		svc.modifProducto(id, producto);
+		if(cant.intValue()>0){
+			svc.modifCant(producto.getCodigo(), cant);
+		}
+		
+	}
+	
+//	solo modifica cantidad
+	@Path("/items/{idProducto}")
+	@PUT
+	public void updateStockItemCant(@PathParam("idProducto") String id,
+									@QueryParam("cant") Long cant){
+		
+		if(cant.intValue()>0){
+			svc.modifCant(id, cant);
+		}
+	}
+	
+	@Path("/items/{idProducto}")
+	@DELETE
+	public void deleteStockItem(@PathParam("idProducto") String id){
+		
+		svc.quitarItem(id);
+		
+	}
+	
 }
--- carrito/src/main/java/ar/com/ibm/applegarage/carrito/controller/CarritoController.java
+++ carrito/src/main/java/ar/com/ibm/applegarage/carrito/controller/CarritoController.java
@@ -1,5 +1,76 @@
 package ar.com.ibm.applegarage.carrito.controller;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import ar.com.ibm.applegarage.carrito.model.Carrito;
+import ar.com.ibm.applegarage.carrito.service.CarritoService;
+
+@Path("/carrito")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CarritoController {
 
+	private CarritoService svc = new CarritoService();
+	
+//	devuelve un resumen del carrito, todavia no se en que formato, 
+//	pero deberia incluir la lista de items+cantidad+total_de_linea, subtotal, tickets de promocion,
+//	costo de envio, etc
+	@GET
+	public void getCarritoInfo(){
+		
+	}
+	
+//	recibe un objeto Carrito (quizas si el cliente lo conserva en una cookie y quisiera
+//	recuperarlo al loguearse
+	@POST
+	public void setCarrito(Carrito carrito){
+		
+//		svc.setCarrito(carrito);
+		
+	}
+	
+//	devuelve los items del carrito + cantidad con su total de linea
+	@Path("/items")
+	@GET
+	public void getCarritoItems(){
+		
+	}
+	
+	@Path("/items/{idProducto}")
+	@POST
+	public void addCarritoItem(@PathParam("idProducto") String id,
+								@QueryParam("cant") Long cant){
+		if(cant.intValue()>0){
+			svc.agregarItem(id, cant);
+		}else{
+			svc.agregarItem(id, 1L);
+		}
+	}
+	
+	@Path("/items/{idProducto}")
+	@PUT
+	public void updateCarritoItemCant(@PathParam("idProducto") String id,
+										@QueryParam("cant") Long cant){
+		if(cant.intValue()>0){
+			svc.cambiarCant(id, cant);
+		}
+		
+	}
+	
+	@Path("/items/{idProducto}")
+	@DELETE
+	public void deleteCarritoItem(@PathParam("idProducto") String id){
+		
+		svc.eliminarItem(id);
+	
+	}
 }
